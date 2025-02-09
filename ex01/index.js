07c027dcdcf8ddd1621953d8d61afa7a72503d70
wizdom972 <jihae0584@gmail.com>
2025-02-10 08:58:58 +0900

    📝 2025-02-07 수업 내용

--- ex01/index.js
+++ ex01/index.js
@@ -1,2 +1,6 @@
-const babel = require('@babel/core');
+const babel = require("@babel/core");
 
+const source = "const fn = () => {}";
+const result = babel.transform(source, {});
+
+console.log(result);
